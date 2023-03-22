dbe1b2bb1eabdc990064547c0fbb04746bde283b
parker <parkerschellenberg@gmail.com>
2023-03-22 17:56:01 -0500

    Add myprogram

---
+++ myprogram.hs
@@ -0,0 +1,9 @@
+-- Define a function to calculate the sum of two integers
+add :: Int -> Int -> Int
+add x y = x + y
+
+-- Call the add function with two integers
+result = add 10 10
+
+-- Print the result to the console
+main = putStrLn (show result)
