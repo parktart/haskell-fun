18add89d426a19f2d40a6dab5c6f4e76ef1a990c
parker <parkerschellenberg@gmail.com>
2023-03-23 08:23:36 -0500

    Add solutions

--- haskellround.hs
+++ haskellround.hs
@@ -1,5 +1,29 @@
-roundToTwoDecimalPlaces :: Double -> Double
-roundToTwoDecimalPlaces x = (fromIntegral (round (x * 100))) / 100
+-- roundToTwoDecimalPlaces :: Double -> Double
+-- roundToTwoDecimalPlaces x = (fromIntegral (round (x * 100))) / 100
 
--- Example usage:
-roundedNumber = roundToTwoDecimalPlaces 0.145
+-- roundedNumber = roundToTwoDecimalPlaces 0.145
+
+-- main :: IO ()
+-- main = putStrLn (show roundedNumber)
+
+
+-- roundToTwoDecimalPlaces :: Double -> Double
+-- roundToTwoDecimalPlaces x = fromIntegral (round (x * multiplier)) / multiplier
+--  where
+--    multiplier = 10 ^ precision
+--    precision = 2
+
+-- main :: IO ()
+-- main = print (roundToTwoDecimalPlaces 0.145)
+
+
+import Data.Number.CReal
+
+roundToTwoDecimalPlaces :: Double -> Double
+roundToTwoDecimalPlaces x = fromRational (toRational (roundTo 2 (toRational x)))
+
+main :: IO ()
+main = print (roundToTwoDecimalPlaces 0.145)
+
+main :: IO ()
+main = print (roundToTwoDecimalPlaces 0.145)
