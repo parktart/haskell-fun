25e918c58107845898b3d24bff77831c77949bd1
parker <parkerschellenberg@gmail.com>
2023-03-23 14:31:26 -0500

    Add javascript function

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
-function roundAccurately(num) {
+function roundAccuratelyToTwo(num) {
   return Number(Math.round(num + 'e2') + 'e-2')
 }
 
-console.log(roundAccurately(0.145))
+console.log(roundAccuratelyToTwo(0.145)) // 0.15 Yay!
+
+
+function roundAccurately(num, precision) {
+  return Number(Math.round(num + 'e' + precision) + 'e' + precision * -1)
+}
+
+roundAccurately(1.005, 2) // 1.01 Yay!
+roundAccurately(12345, -2) // 12300
+console.log(roundAccurately(0.145, 2)) // 0.15 Yay!
